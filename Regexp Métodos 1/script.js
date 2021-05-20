b743d2df0d8658ebb2c687da45a7df6445b88f66
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2021-05-20 19:51:26 -0300

    Regexp

---
+++ Regexp Métodos 1/script.js
@@ -0,0 +1,23 @@
+// const regexp = /\w+/gi;
+// const regexp1 = new RegExp('\\d+', 'gi');
+// console.log(frase.replace(regexp1, 'X'))
+
+// const frase = 'JavaScript e Java Linguagem 007 Java'
+// const regexp = /Java/g;
+
+// let i = 0
+// while (regexp.test(frase)) {
+//   console.log(i++, regexp.lastIndex)
+// }
+
+// const frase = 'JavaScript, TypeScript, CoffeeScript, Java'
+
+// const regexp = /\w+/g;
+
+// const resultados = frase.match(regexp)
+// console.log(resultados)
+
+
+
+
+
